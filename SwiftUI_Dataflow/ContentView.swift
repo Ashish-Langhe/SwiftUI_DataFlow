2c1191142535e81ae589cc9168d67bf5ef048f41
Ashish Langhe <ashish.langhe@globant.com>
2023-11-10 00:08:25 +0530

    Initial Commit

---
+++ SwiftUI_Dataflow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Dataflow
+//
+//  Created by Ashish Langhe on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Dataflow/SwiftUI_DataflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DataflowApp.swift
+//  SwiftUI_Dataflow
+//
+//  Created by Ashish Langhe on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DataflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
